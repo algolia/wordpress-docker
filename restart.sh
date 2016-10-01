81ea02eed4f5876dad7fb1b22f04b7848ae6ca5b
Raymond Rutjes <raymond.rutjes@gmail.com>
2016-10-01 16:41:41 +0200

    Save wordpress directory before changing anything else

--- restart.sh
+++ restart.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 
+# Store a backup of the current WordPress folder in case something goes wrong.
+timestamp=$(date +%s)
+cp -R wordpress backups/ ${timestamp}
+
+# Clean the wordpress folder and keep the plugins & themes.
 rm -Rf ./tmp
 mkdir ./tmp
 cp -R ./wordpress/wp-content/plugins ./tmp/plugins
@@ -10,6 +15,7 @@ mkdir -p ./wordpress/wp-content
 mv ./tmp/plugins ./wordpress/wp-content/plugins
 mv ./tmp/themes ./wordpress/wp-content/themes
 
+# Start docker containers.
 docker-compose stop
 docker-compose rm --all --force
 docker-compose build
